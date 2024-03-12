fd8c1ba154cb37d9eb51ae0e2c77ea8f3e43b3e4
Abhinand KR <nanduttanvsabhi@gmail.com>
2024-03-12 16:43:23 +0530

    add generic json response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // application status, environment and version
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment %s\n", app.cfg.Env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data := map[string]string{
+		"status": "available",
+		"environment": app.cfg.Env,
+		"version": version,
+	}
 
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.cfg.Env, version)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		http.Error(w, "The server encountered an error and could not process the request ", http.StatusInternalServerError)
+		return
+	}
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"abhinandkakkadi/functional/internals/data"
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
+	"time"
 )
 
 
@@ -17,13 +16,25 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	// When httprouter is parsing a request, any interpolated URL parameters will be 
 	// stored in the request context. We can use the ParamsFromContext() function to 
 	// retrieve a slice containing these parameter names and values.
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	id, err := app.readIDParam(r)
 	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie%d\n", id)
+	movies := data.Movie{
+		ID: id,
+		CreatedAt: time.Now(),
+		Title: "Casablanca",
+		Runtime: 102,
+		Genres: []string{"drama", "romance", "war"},
+		Version: 1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movies, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process our request", http.StatusInternalServerError)
+	}
+
 }
