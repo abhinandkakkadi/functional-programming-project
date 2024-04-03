4af8839d5c3ce93fede464729b4eec0efcf5e77f
Abhinand KR <nanduttanvsabhi@gmail.com>
2024-04-03 15:30:45 +0530

    add reading error

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -39,4 +42,48 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	// decode the request body into target destination
+	err := json.NewDecoder(r.Body).Decode(dst)
+	if err != nil {
+		// If there is an error during decoding, start the triage...
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+		// Use the errors.As() function to check whether the error has the type
+		// *json.SyntaxError. If it does, then return a plain-english error message
+		// which includes the location of the problem.
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly formed JSON (at character %d)", syntaxError.Offset)
+
+			// In some circumstances Decode() may also return an io.ErrUnexpectedEOF error
+			// for syntax errors in the JSON. So we check for this using errors.Is() and
+			// return a generic error message. There is an open issue regarding this at
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly formatted JSON")
+
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+
+		// An io.EOF error will be returned by Decode() if the request body is empty. We
+		// check for this with errors.Is() and return a plain-english error message
+		// instead.
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+
+		default:
+			return err
+		}
+
+	}
 }
--- internals/data/movies.go
+++ internals/data/movies.go
@@ -10,7 +10,7 @@ type Movie struct {
 	// Use the Runtime type instead of int32. Note that the omitempty directive will
 	// still work on this: if the Runtime field has the underlying value 0, then it will // be considered empty and omitted -- and the MarshalJSON() method we just made
 	// won't be called at all.
-	Runtime   Runtime     `json:"runtime,omitempty"` // Represent Runtime in string rather than in number when converted into JSON
-	Genres    []string  `json:"genres,omitempty"`
-	Version   int32     `json:"version"`
+	Runtime Runtime  `json:"runtime,omitempty"` // Represent Runtime in string rather than in number when converted into JSON
+	Genres  []string `json:"genres,omitempty"`
+	Version int32    `json:"version"`
 }
